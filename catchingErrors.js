7b6b8d3c453eb8bfd1abc7c83c4c54c1adc3988a
michael ganesan <cookieganesan@gmail.com>
2018-03-18 21:52:59 -0400

    Added code for observable that returns a observable for error

--- catchingErrors.js
+++ catchingErrors.js
@@ -2,8 +2,11 @@
 exports.__esModule = true;
 var Observable_1 = require("rxjs/Observable");
 require("rxjs/add/observable/range");
+var Rxjs = require("rxjs");
 var getJson = function (data) {
-    return (Observable_1.Observable.from(data).map(function (x) { return JSON.parse; }));
+    return (Observable_1.Observable.from(data).map(function (x) { return JSON.parse(x); }));
 };
-var data = ['{"1": 1, "2": 2}', '{"1: 1}'];
-var caught$ = getJson(data)["catch"](Observable_1.Observable.of({ error: "Sorry JSON Error" }));
+var caught$ = getJson(['{"1": 1, "2": 2}', '{"1:1}'])["catch"](function (val) { return Rxjs.Observable.of("I caught " + val + " error"); });
+caught$.subscribe(function (json) { return console.log("Parsed JSON: ", json); }, function (error) { return console.log(error.message); }, function () {
+    console.log("completed");
+});
--- catchingErrors.ts
+++ catchingErrors.ts
@@ -3,17 +3,20 @@ import "rxjs/add/observable/range";
 
 import * as Rxjs from 'rxjs';
 
-let getJson = (data:any)=>{
-     
-    return(Observable.from(data).map(x=> JSON.parse));
+let getJson = (data:any)=>{   
+    return(Observable.from(data).map(x=> JSON.parse(<string>x)))}
 
-}
+const caught$ = getJson(['{"1": 1, "2": 2}'​, ​'{"1:1}']).catch(val => Rxjs.Observable.of(`I caught ${val} error`));
 
-let data = ['{"1": 1, "2": 2}'​, ​'{"1: 1}'];
 
-const caught$ = getJson(data).catch(
-     Observable.of({error: "Sorry JSON Error"}));
+caught$.subscribe(json=>console.log("Parsed JSON: ",json),
+(error)=>console.log(error.message),()=>{
+    console.log("completed");
 
+});
+
+
+    
 
 
 
