86d4b40c5db001be7ac556341a3e76a19773dee9
Michael Ganesan <cookieganesan@gmail.com>
2018-03-22 21:54:35 -0400

    Subject example

--- Subject.js
+++ Subject.js
@@ -1,3 +1,13 @@
 "use strict";
 exports.__esModule = true;
-require("rxjs");
+var rxjs_1 = require("rxjs");
+var array = [];
+var arraySubject = new rxjs_1.Subject();
+var data$ = rxjs_1.Observable.interval(800);
+data$.subscribe(function (x) {
+    array.push(x);
+    arraySubject.next(x);
+});
+arraySubject.subscribe(function (x) {
+    console.log("Array length " + array.length);
+});
--- Subject.ts
+++ Subject.ts
@@ -1,3 +1,31 @@
-import { Observable,Subject } from 'rxjs/Observable';
-import 'rxjs';
+//import { Observable } from 'rxjs/Observable';
+import { } from 'rxjs/es'
 import * as colors from 'colors'
+import { Subject,Observable } from 'rxjs';
+
+let array:number[] = [];
+
+let arraySubject = new Subject();
+
+let data$ = Observable.interval(800);
+
+
+data$.subscribe(x => {
+      array.push(x);
+      arraySubject.next(x);
+});
+
+arraySubject.subscribe(x => {
+    console.log(`Array length ${array.length}`);
+
+});
+
+
+
+
+
+
+
+
+
+
