24b482f09a8e98f65ef4982db77a8e97bdbd2020
Michael Ganesan <cookieganesan@gmail.com>
2018-04-03 16:55:01 -0400

    groupby observer

---
+++ Schedular.js
@@ -0,0 +1,69 @@
+"use strict";
+exports.__esModule = true;
+/* ===========================
+   Description: Schedular
+   Date: 04/02/2018
+   Notes: Sample application using a schedular
+ 
+   =========================== */
+var Observable_1 = require("rxjs/Observable");
+require("rxjs/add/observable/range");
+require("rxjs/add/operator/groupBy");
+require("rxjs/add/operator/map");
+require("rxjs/add/observable/from");
+require("rxjs/add/operator/observeOn");
+var asap_1 = require("rxjs/scheduler/asap");
+//Create a sample array
+var myArray = [];
+var displayArray = function () {
+    myArray.forEach(function (element) {
+        console.log("" + element);
+    });
+};
+var displayAsync = function (display) {
+    var currentTime = Date.now();
+    Observable_1.Observable.from(myArray, asap_1.asap).subscribe(function (x) {
+        if (display) {
+            console.log("Value: " + x);
+        }
+    }, function (error) {
+        console.log("Error Message: " + error.message);
+    }, function () {
+        console.log("Total Time: " + (Date.now() - currentTime) + "ms");
+    });
+    console.log("Hi there from Asynchronous!");
+};
+var displaySynchronous = function (display) {
+    var currentTime = Date.now();
+    Observable_1.Observable.from(myArray).subscribe(function (x) {
+        if (display) {
+            console.log("Value: " + x);
+        }
+    }, function (error) {
+        console.log("Error Message: " + error.message);
+    }, function () {
+        console.log("Total Time: " + (Date.now() - currentTime) + "ms");
+    });
+    console.log("Hi there from synchronous! ");
+};
+var counter = 0;
+//map the values
+var displayObserveOn = function () {
+    Observable_1.Observable.from(myArray).groupBy(function (value) { return (value % 2) === 0; })
+        .map(function (value) { return value.observeOn(asap_1.asap); })
+        .subscribe(function (groupObserver$) {
+        console.log("Your group is: " + ++counter);
+        groupObserver$.subscribe(function (x) {
+            console.log("" + x);
+        });
+    });
+};
+var MAX_RANGE = 50;
+Observable_1.Observable.range(1, MAX_RANGE).subscribe(function (x) {
+    myArray.push(x);
+}, function (error) {
+    console.log("Error fill the array: " + error.message);
+}, function () {
+    console.log("Initialized the array");
+    displayObserveOn();
+});
---
+++ Schedular.ts
@@ -0,0 +1,127 @@
+import { Subject } from 'rxjs';
+
+/* ===========================
+   Description: Schedular 
+   Date: 04/02/2018
+   Notes: Sample application using a schedular
+ 
+   =========================== */
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/range';
+import 'rxjs/add/operator/groupBy';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/from';
+import 'rxjs/add/operator/observeOn';
+import { Scheduler } from 'rxjs/Scheduler';
+import { asap } from 'rxjs/scheduler/asap';
+
+//import 'rxjs';
+import * as colors from 'colors';
+import { toArray } from 'rxjs/operator/toArray';
+import { groupBy } from 'rxjs/operator/groupBy';
+
+
+//Create a sample array
+const myArray: Array<number> = [];
+
+let displayArray = () => {
+
+    myArray.forEach(element => {
+        console.log(`${element}`);
+
+    });
+
+
+}
+
+
+let displayAsync = (display: boolean) => {
+
+    const currentTime = Date.now();
+    Observable.from(myArray, asap).subscribe((x: number) => {
+        if (display) {
+            console.log(`Value: ${x}`);
+        }
+    }, (error: Error) => {
+
+        console.log(`Error Message: ${error.message}`);
+
+    }, () => {
+
+        console.log(`Total Time: ${Date.now() - currentTime}ms`);
+
+    });
+
+    console.log("Hi there from Asynchronous!");
+}
+
+let displaySynchronous = (display: boolean) => {
+
+    const currentTime = Date.now();
+    Observable.from(myArray).subscribe((x: number) => {
+        if (display) {
+            console.log(`Value: ${x}`);
+        }
+
+    }, (error: Error) => {
+
+        console.log(`Error Message: ${error.message}`);
+
+    }, () => {
+
+        console.log(`Total Time: ${Date.now() - currentTime}ms`);
+
+    });
+
+    console.log("Hi there from synchronous! ");
+}
+
+let counter=0;
+//map the values
+let displayObserveOn = () => {
+
+    Observable.from(myArray).groupBy(value => (value % 2) === 0)
+    .map(value => value.observeOn(asap))
+    .subscribe(groupObserver$ =>{
+       console.log(`Your group is: ${++counter}`);
+       groupObserver$.subscribe(x=>{
+           console.log(`${x}`);
+       });
+
+
+    });
+    
+}
+
+
+
+const MAX_RANGE: number = 50;
+
+Observable.range(1, MAX_RANGE).subscribe((x: number) => {
+
+    myArray.push(x);
+
+}, (error: Error) => {
+    console.log(`Error fill the array: ${error.message}`)
+}, () => {
+    console.log("Initialized the array");
+
+    displayObserveOn();
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
